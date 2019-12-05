22411d35b68129fd33a35aa0b3dee909137cc1ef
ShanazFC <schowdhery09@gmail.com>
2019-12-05 00:08:44 -0500

    fixed
    fixed font awesome integration
    created escape rooms page
    testing contact form

--- php/contact.php
+++ php/contact.php
@@ -16,7 +16,7 @@ $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
   //
-  mail( "schowdhery09@gmail.coms", $_POST['subject'], $_POST['message'], $headers );
+  mail( "schowdhery09@gmail.com", $_POST['subject'], $_POST['message'], $headers );
 
   //      ^
   //  Replace with your email
